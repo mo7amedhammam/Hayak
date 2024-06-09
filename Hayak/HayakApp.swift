5bd5f7a8b3e3b043846b480f8ba29a46e63e5517
mohamed.hammam <mohamed.hammam@wecancity.org>
2024-06-09 13:22:59 +0300

    Initial Commit

---
+++ Hayak/HayakApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayakApp.swift
+//  Hayak
+//
+//  Created by wecancity on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HayakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
