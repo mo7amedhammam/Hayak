39db5d98653070b2e4b00257b89bfd5b35fc4344
mohamed hammam <mohamed.hammam.pro@gmail.com>
2026-04-11 14:54:37 +0200

    App renamed to Bridgeble

---
+++ Bridgeble/AppModules/AppViews/OnBoarding/WellcomeLoginScreen.swift
@@ -0,0 +1,130 @@
+//
+//  WellcomeLoginScreen.swift
+//  Hayak
+//
+//  Created by Mohamed Ragab on 03/07/2024.
+//
+
+import SwiftUI
+
+struct WellcomeLoginScreen: View {
+    
+    @State var isActive : Bool = false
+    //    @State var isActiveSignUp : Bool = false
+    //    @State var isActiveHome : Bool = false
+    @State var destination : AnyView = AnyView(EmptyView())
+    
+    var body: some View {
+        
+        
+        ZStack(alignment : .bottom) {
+            Color(.bg).ignoresSafeArea()
+                .navigationBarBackButtonHidden(true)
+            
+            NavigationLink(
+                destination: destination,
+                isActive: $isActive,
+                label: {
+                    EmptyView()
+                }
+            )
+            
+            //            NavigationLink(
+            //                destination: SignUpScreen().navigationBarBackButtonHidden(true),
+            //                isActive: $isActiveSignUp,
+            //                label: {
+            //                    EmptyView()
+            //                }
+            //            )
+            
+            VStack {
+                
+                Image("wellcome1")
+                    .frame(width: 300 , height: 170)
+                Image("wellcome2")
+                    .frame(width: 260 , height: 200)
+                
+                Spacer()
+                ZStack (alignment : .center)  {
+                    Color.clear.ignoresSafeArea()
+                    Image(.imgbg)
+                        .resizable()
+                        .ignoresSafeArea()
+                    
+                    VStack (spacing : 20){
+                        
+                        Text("Welcome to Bridgeble")
+                            .font(.Bold(size: 18))
+                            .foregroundColor(.white)
+                            .padding(.top , 20)
+                        
+                        VStack {
+                            Text("Restaurants and search by cuisine, price,")
+                                .font(.Medium(size: 13))
+                                .foregroundColor(.white)
+                                .frame(height: 16)
+                            
+                            Text("location, and more.")
+                                .font(.Medium(size: 13))
+                                .foregroundColor(.white)
+                                .frame(height: 16)
+                        }
+                        
+                        Button(action: {
+                            // get started login
+                            destination = AnyView( SignInScreen().navigationBarBackButtonHidden(true)
+                            )
+                            self.isActive = true
+                        }, label: {
+                            Text("Sign in")
+                                .frame(height: 50) // Set the height here
+                                .frame(maxWidth: .infinity)
+                                .font(.Medium(size: 14))
+                                .foregroundColor(.main).background(.bg)
+                                .cornerRadius(10)
+                        })
+                        
+                        Button(action: {
+                            // get started login
+                            destination = AnyView( SignUpScreen().navigationBarBackButtonHidden(true)
+                            )
+                            self.isActive = true
+                        }, label: {
+                            Text("Sign Up")
+                                .frame(height: 50) // Set the height here
+                                .frame(maxWidth: .infinity)
+                                .font(.Medium(size: 14))
+                                .foregroundColor(.main).background(.bg)
+                                .cornerRadius(10)
+                        })
+                        Button(action: {
+                            // go to home
+                            destination = AnyView( TabViewWithCenterBtn().navigationBarBackButtonHidden(true)
+                            )
+                            isActive = true
+                            
+                        }, label: {
+                            Text("Go To Home")
+                                .frame(height: 50) // Set the height here
+                                .frame(maxWidth: .infinity)
+                                .font(.Medium(size: 14))
+                                .foregroundColor(.main).background(.bg)
+                                .cornerRadius(10)
+                        })
+                        
+                    }
+                    .padding(16)
+                    
+                }
+                .frame(height: 330)
+            }
+            
+            
+        }.hideNavigationBar()
+            .localizeView()
+    }
+}
+
+#Preview {
+    WellcomeLoginScreen()
+}
